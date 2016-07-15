55a3ce64a7a6da9d4b8b36bb06e69b0ac753853c
Jason McCallister <themccallister@gmail.com>
2016-07-15 14:42:01 -0400

    Add clean and rename build-images script

--- images.sh
+++ images.sh
@@ -1,5 +1,5 @@
 # build the 7.0-apache image
-docker build -t venveo/baseimage:7.0-apache ./php-apache
+# docker build -t venveo/baseimage:7.0-apache ./php-apache
 
 # build the 5.7-mysql image
 docker build -t venveo/baseimage:5.7-mysql ./mysql
@@ -9,3 +9,9 @@ docker build -t venveo/baseimage:1.4-memcached ./memcached
 
 # build the 3.2-redis image
 docker build -t venveo/baseimage:3.2-redis ./redis
+
+# build the 1.10-nginx image
+docker build -t venveo/baseimage:1.10-nginx ./nginx
+
+# build the 7.0-php-fpm image
+docker build -t venveo/baseimage:7.0-php-fpm ./php-fpm
